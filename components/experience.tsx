e268e9fb40cf252cacc7c13018258c04ccd1f479
Dinh <0306201403@caothang.edu.vn>
2023-09-11 16:42:04 +0700

    t4

--- components/experience.tsx
+++ components/experience.tsx
@@ -25,8 +25,22 @@ export function DefaultTimeline() {
             <div>
               T4TEK COMPANY LIMITED
               <p style={{ color: "gray" }}>Front-end developer</p>
-              <p>• My main job is designing and building websites</p>
-              <p>• Server management</p>
+              <p>• My main job is designing and building websites.</p>
+              <p>• Server management.</p>
+              <p>• Execute specialized tasks according to the assigned plan.</p>
+              <p>
+                • Receive training and conduct research in the relevant field.
+              </p>
+              <p>• Participate in training courses and seminars as guided. </p>
+              <p>
+                • Create documentation, slides, and present the work undertaken
+                and research findings. Create documentation, slides, and present
+                the work undertaken and research findings.
+              </p>
+              <p>
+                • Proactively report on work progress according to the
+                established plan.
+              </p>
             </div>
           </TimelineBody>
         </TimelineItem>
